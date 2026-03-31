13a0df4e2cc3f99c21027f135196a0f4c69f3660
jinguihua <156711203@qq.com>
2026-03-31 14:51:00 +0800

    changed

---
+++ base_test.go
@@ -0,0 +1,14 @@
+package myphonenumber
+
+import "testing"
+
+func Test_Phone(t *testing.T) {
+	//t.Log("check", Check("86", ""))
+	//t.Log("CheckPhone", CheckPhone("+"))
+	//t.Log("GetCountryCode", GetCountryCode(""))
+	//t.Log("FormatToInternational", FormatToInternational(""))
+	//ret, err := FormatToE164("86", "")
+	//t.Log("FormatToE164", ret, err)
+
+	//t.Log("MaskPhoneNumber:", MaskPhoneNumber("+"))
+}
---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/any-call/myphonenumber
+
+go 1.24.2
+
+require (
+	github.com/nyaruka/phonenumbers v1.6.12 // indirect
+	golang.org/x/exp v0.0.0-20250305212735-054e65f0b394 // indirect
+	golang.org/x/text v0.23.0 // indirect
+	google.golang.org/protobuf v1.36.11 // indirect
+)
